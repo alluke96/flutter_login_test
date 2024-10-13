ca67b70054153f80474daaec5b58fed636b65376
Allyson Luan Dunke <allyson.dunke@gmail.com>
2024-10-13 15:39:16 -0300

    Initial commit
    Project creation + basic configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_login_bloc
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_login_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dunke.flutter_login_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
